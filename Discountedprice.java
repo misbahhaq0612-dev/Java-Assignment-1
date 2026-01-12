cb6d3defe57f473fd682f5d478e1c2c6ff7d30ad
Misbah <misbahhaq0612@gmail.com>
2026-01-12 18:58:58 +0500

    Add files via upload

---
+++ Discountedprice.java
@@ -0,0 +1,19 @@
+
+public class Discountedprice {
+    
+    public static void main(String[] args) {
+        double csp=4500;
+        double dp=25;
+        double da;
+        double dsp;
+        
+         da=csp*dp/100;
+         dsp=csp-da;
+       
+       System.out.println("Price after discount " + dsp);
+       
+       
+        
+    }
+}
+
