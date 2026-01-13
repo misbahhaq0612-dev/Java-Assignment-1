07de653057b5ccd18c5ee30f8b084d7e3e4adada
Misbah <misbahhaq0612@gmail.com>
2026-01-13 20:16:12 +0500

    Add files via upload

---
+++ Watts.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Watts {
+    public static void main (String[]args){
+     double watt;
+     double amp;
+     double volt;
+
+     Scanner read = new Scanner(System.in);
+
+     System.out.print("Enter voltage: ");
+     volt= read. nextDouble();
+     System.out.print("Enter ampere: ");
+     amp= read. nextDouble();
+
+     watt= volt*amp;
+     System.out.print("Watts: "+ watt);
+     
+     read.close();
+
+    }
+    
+}
